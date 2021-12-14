bc9eea1f58b32030a4f7dc3c88c91dfc0f3122da
川崎澄明 <cb_clear_bright@yahoo.co.jp>
2021-12-14 14:50:14 +0000

    Rowコンポーネントの作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Row } from "./compornents/Row";
+import { requests } from "./request";
+
 
 function App() {
   return (
     <div className="App">
+      <Row
+        title="NETFLIX ORIGUINALS"
+        fetchUrl={requests.feachNetflixOriginals}
+        isLargeRow
+      />
+      <Row title="Top Rated" fetchUrl={requests.feactTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.feactActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.feactComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.feactHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.feactRomanceMovies} />
+      <Row title="DOcumentaries" fetchUrl={requests.feactDocumentMovies} />
     </div>
   );
 }
