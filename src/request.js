1ccfe06c3b9ec53ca16da98c7ff7f743b7cc2880
川崎澄明 <cb_clear_bright@yahoo.co.jp>
2021-12-14 18:46:01 +0900

    API取得の下準備

---
+++ src/request.js
@@ -0,0 +1,12 @@
+const API_KEY = "8ed0c5c9eb1557505e58e8e2c18dc5d3";
+
+export const requests ={
+    feachTrending:`/trending/all/week?api_key=${API_KEY}&language=en-us`,
+    feachNetflixOriginals:`/discover/tv?api_key=${API_KEY}&with_networks=213`,
+    feactTopRated:`/discover/tv?api_key=${API_KEY}&languager=en-us`,
+    feactActionMovies:`/discover/tv?api_key=${API_KEY}&with_genres=28`,
+    feactComedyMovies:`/discover/tv?api_key=${API_KEY}&with_genres=35`,
+    feactHorrorMovies:`/discover/tv?api_key=${API_KEY}&with_genres=27`,
+    feactRomanceMovies:`/discover/tv?api_key=${API_KEY}&with_genres=10749`,
+    feactDocumentMovies:`/discover/tv?api_key=${API_KEY}&with_genres=99`,
+}
